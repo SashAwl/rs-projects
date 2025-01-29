52d52534787911d00325c9ee9518bfd9fce4eaaf
Alexandra Zaporenko <aleks.shilova@gmail.com>
2025-01-29 23:11:02 +0300

    init: start nonograms task

---
+++ nonograms/src/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: `
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>My Minimal Project</title>
+                </head>
+                <body>
+                    <script src="bundle.js"></script>
+                </body>
+                </html>
+            `,
+      inject: false,
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
