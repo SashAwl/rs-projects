e329c7892f2b3a2294d526d612e00fa855f79bb4
Alexandra Zaporenko <aleks.shilova@gmail.com>
2025-02-08 00:27:08 +0300

    fix: correct webpack config

--- nonograms/webpack.config.js
+++ nonograms/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/scripts/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist', 'nonograms'),
     clean: true,
   },
   mode: 'development',
