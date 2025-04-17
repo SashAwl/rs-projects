442c63e3c144fa12cf0210dca65a307f96fe838f
Alexandra Zaporenko <aleks.shilova@gmail.com>
2025-04-17 22:54:01 +0300

    feat: complete project configuration

--- fun-chat/webpack.config.js
+++ fun-chat/webpack.config.js
@@ -9,7 +9,7 @@ export default {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/sashawl-JSFE2024Q4/async-race/',
+    publicPath: '/sashawl-JSFE2024Q4/fun-chat/',
     clean: true,
   },
   resolve: {
