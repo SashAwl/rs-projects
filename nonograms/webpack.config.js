5ed412ee8e3a6b1834e5ec7cb0bee45b80200934
Alexandra Zaporenko <aleks.shilova@gmail.com>
2025-05-06 19:08:04 +0300

    fix: correct public path

--- nonograms/webpack.config.js
+++ nonograms/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/sashawl-JSFE2024Q4/nonograms/',
+    publicPath: '/rs-projects/nonograms/',
     clean: true,
   },
   mode: 'development',
